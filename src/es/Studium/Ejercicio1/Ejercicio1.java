4022bbb1f92c27e0c532444bdd131e414ba5db8c
AlvCarballo <alv.carballo@outlook.es>
2020-01-13 13:09:26 +0100

    Programa con error

---
+++ src/es/Studium/Ejercicio1/Ejercicio1.java
@@ -0,0 +1,13 @@
+package es.Studium.Ejercicio1; 
+public class Ejercicio1 
+{
+	public static void main(String[] args) 
+	{
+		boolean esCierto = false; 
+		while(esCierto = true) 
+		{
+			System.out.println("Pues sí que es cierto");
+		}
+		System.out.println("Pues no que es cierto"); 
+	}
+}
